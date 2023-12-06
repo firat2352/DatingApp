097cc4fec959ecb504c7560050238fc7c7db5741
firat2352 <firatcelik52@gmail.com>
2023-12-06 22:01:24 +0300

    exception handling middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,6 @@
-using System.Text;
-using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using API.Middleware;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,38 +10,13 @@ builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
-// builder.Services.AddDbContext<DataContext>(opt => //moved to service collection
-// {
-//     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-// });
-
-
-// builder.Services.AddCors();
-// builder.Services.AddScoped<ITokenService,TokenService>(); moved to AppExtensions
-
-// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme). //moved to IdentityServiceExtension
-//             AddJwtBearer(options =>
-//             {
-//                 options.TokenValidationParameters = new TokenValidationParameters //how to validate the token for API
-//                 {
-//                     ValidateIssuerSigningKey = true,
-//                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
-//                     ValidateIssuer = false,
-//                     ValidateAudience = false
-//                 };
-//             });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-//app.UseHttpsRedirection();
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));//before author
+
 app.UseAuthentication();
 app.UseAuthorization();
 
